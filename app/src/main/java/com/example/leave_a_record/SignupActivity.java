ae31def7d8e4bb1db151eb6158e8544fad932a81
CHOI97 <32347874+CHOI97@users.noreply.github.com>
2020-10-09 03:00:33 +0900

    create Sign-up

---
+++ app/src/main/java/com/example/leave_a_record/SignupActivity.java
@@ -0,0 +1,69 @@
+package com.example.leave_a_record;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignupActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private static final String TAG="SignupActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.Login_bt).setOnClickListener(onClickListener);
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.Login_bt:
+                    signUp();
+                    break;
+            }
+        }
+    };
+
+    private void signUp() {
+        String email =((EditText)findViewById(R.id.Login_id)).getText().toString();
+        String password = ((EditText)findViewById(R.id.Login_pw)).getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+}
